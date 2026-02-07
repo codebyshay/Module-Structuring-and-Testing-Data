c82ecac9fc4918688f9425ff7736fe9870208b43
Shuheda <shuheda.hatun@gmail.com>
2026-02-07 16:21:46 +0000

    Did 1-count.js

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// This line (line 3), is updating the value of the count variable. The = operator is an assignment operator which assigns the value on the right (count + 1) to the variable on the left (count). 
