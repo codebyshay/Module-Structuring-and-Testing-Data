704d37236aa79d06283fa895d12b2461d49847f2
Shuheda <shuheda.hatun@gmail.com>
2026-02-10 21:16:10 +0000

    Console and const
    Swapped the position of const from under the console to on top of the console to allow javascript to read it in order.

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,7 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+// The error happens because cityOfBirth is used before it is defined. JavaScript runs code from top to bottom, so the variable must be declared first.
