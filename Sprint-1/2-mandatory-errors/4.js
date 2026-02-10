c611ae5b5d300a57bfc152cf570e8e15092c20ad
Shuheda <shuheda.hatun@gmail.com>
2026-02-10 21:40:57 +0000

    Update 4.js
    SyntaxError fixed..twelve instead of 12 and twentyFour instead of 24

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,11 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
+
+// Variables cannot start with a number.
+// When JavaScript tries to run the code, it throws a SyntaxError.
+// To fix the error we can rename the variable to start with a letter, a underescore_ or a dollar sign $ instead of a number.
+
+const twelveHourClockTime = "8:53";
+const twentyFourHourClockTime = "08:53";
+
+// I changed the time to match the 12 and 24 hr clock format.
