c557dca9afd4b67f64c53ccf81f4a0b91a2b6f25
Shuheda <shuheda.hatun@gmail.com>
2026-02-10 21:09:31 +0000

    const to let
    let allows variable age, const does not.

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,8 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
+console.log(age);
+
+// The code above will output 34, because we are reassigning the value of age to be the current value of age plus 1. So, 33 + 1 = 34.
+
