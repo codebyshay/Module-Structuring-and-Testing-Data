6223c28b6afe8fbfb5dcc3aa9579694ff732aa2b
Shuheda <shuheda.hatun@gmail.com>
2026-02-19 22:17:07 +0000

    Sprint 2 0.js
    New Code

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,11 +1,11 @@
 // Predict and explain first...write your prediction here
 // I predict, when the code runs, it will print 320, but it will also print "The result of multiplying 10 and 32 is undefined".
 
-function multiply(a, b) {
-  console.log(a * b);
-}
+//function multiply(a, b) {
+//  console.log(a * b);
+// }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // Write your explanation here
 // The function multiply(a, b) logs the result of a * b to the console but does not return a value.
@@ -13,4 +13,12 @@ console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 // Therefore, inside the template literal, the value is undefined, which is why the final output becomes "The result of multiplying 10 and 32 is undefined".
 
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+// Write your new code here
+// To fix the problem, the function should return the result instead of logging it.
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
