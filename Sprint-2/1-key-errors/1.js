794f136aed1f5dcd5f3b98d0ec5fe5021818d567
Shuheda <shuheda.hatun@gmail.com>
2026-02-19 01:30:37 +0000

    Update 1.js
    Code amended and working.

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -5,16 +5,25 @@
 // Try playing computer with the example to work out what is going on
 // When the 'function convertToPercentage(decimalNumber)' is created, Javascript already creates a variable called decimalNumber. then, inside the function, 'const decimalNumber = 0.5' tries to create another variable with the same name. This casues a syntax error because you cannot declare two variables with the same name in the same scope. 
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
-  const percentage = `${decimalNumber * 100}%`;
+// function convertToPercentage(decimalNumber) {
+  // const decimalNumber = 0.5;
+  // const percentage = `${decimalNumber * 100}%`;
 
+  // return percentage;
+// }
+
+// console.log(decimalNumber);
+
+// Write your explanation here
+// I redelcared the parameter decimalNumber using const. 
+// I tried to log decimalNumber outside its scope.
+// The variables declared inside the function are not available outside unless they are returned.
+
+// Finally, correct the code to fix the problem
+
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
   return percentage;
 }
 
-console.log(decimalNumber);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+console.log(convertToPercentage(0.5)); // "50%"
