364c15c8c2f076e932e8651dc6929f6eee45609b
Shuheda <shuheda.hatun@gmail.com>
2026-02-19 01:47:28 +0000

    Update 2.js
    Explanation of syntax error and new code

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,32 @@
 
 // Predict and explain first BEFORE you run any code...
+// This function should square any number but instead we're going to get an error
 
-// this function should square any number but instead we're going to get an error
+// Write your prediction of the error here:
+// I predict that the error will be a syntax error because the parameter '3' is not a valid variable name.
+// When you define a function,, the thing inside the parentahese must be a parameter name, NOT a number.
 
-// =============> write your prediction of the error here
+// function square(3) {
+   // return num * num;
+// }
 
-function square(3) {
+// Write the error message here
+// SyntaxError: Unexpected number
+
+// Explain the error messge here:
+// When defining a function, inside the parentheses, you need to put a parameter name, which is a variable.
+// In this case, '3' is not a valid parameter name because it is a number, not a variable. 
+// This causes a syntax error because Javascript expects a name, not a number.
+
+// Finally, correct the code to fix the problem
+// Write your new code here
+
+function square(num) {
     return num * num;
 }
 
-// =============> write the error message here
+console.log(square(3));  // 9
+console.log(square(5));  // 25
 
-// =============> explain this error message here
-
-// Finally, correct the code to fix the problem
-
-// =============> write your new code here
 
 
