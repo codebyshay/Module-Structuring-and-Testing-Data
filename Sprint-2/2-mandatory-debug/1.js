a6caa895fb60c5a6540ac91f3dac9f6eff2dc1b6
Shuheda <shuheda.hatun@gmail.com>
2026-02-19 22:34:13 +0000

    Sprint 1.js
    New code updated

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,12 +1,12 @@
 // Predict and explain first...write your prediction here
 // I think the code will return undefined.
 
-function sum(a, b) {
-  return;
-  a + b;
-}
+//function sum(a, b) {
+ // return;
+ // a + b;
+//}
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+//console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // Write your explanation here
 // When JavaScript sees the 'return' statement, it immediately stopd the function. It does NOT continue to the next line.
@@ -15,3 +15,9 @@ console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
