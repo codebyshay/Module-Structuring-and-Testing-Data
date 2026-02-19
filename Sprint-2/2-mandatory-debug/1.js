e38cd413c4635bd6523c385329eba3ac024e54d7
Shuheda <shuheda.hatun@gmail.com>
2026-02-19 22:32:30 +0000

    Sprint 2 1.js
    Prediction and explanation

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,5 @@
-// Predict and explain first...
-//  =============> write your prediction here
+// Predict and explain first...write your prediction here
+// I think the code will return undefined.
 
 function sum(a, b) {
   return;
@@ -8,6 +8,10 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// Write your explanation here
+// When JavaScript sees the 'return' statement, it immediately stopd the function. It does NOT continue to the next line.
+// So this part, 'a+b;' never runs. To return the sum of a and b, we need to put it on the same line as the return.
+// Like the: 'return a + b;'
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
