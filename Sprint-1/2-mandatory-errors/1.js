4c474e383038bc8ff8c4ed5535600c29e30910db
Shuheda <shuheda.hatun@gmail.com>
2026-02-21 15:25:08 +0000

    update error

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -5,4 +5,4 @@ age = age + 1;
 console.log(age);
 
 // The code above will output 34, because we are reassigning the value of age to be the current value of age plus 1. So, 33 + 1 = 34.
-
+// The error in the code is that it was trying to reassign a value to a variable that was declared with let. This is not allowed in JavaScript.
