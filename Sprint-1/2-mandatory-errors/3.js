17b2307ccb33fff9e7e2f0d295c50d90b00c50dd
Shuheda <shuheda.hatun@gmail.com>
2026-03-05 11:47:30 +0000

    3.js restored
    Restored 3.js to original text and made a prediction about the use of slice

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,8 +1,14 @@
-// Currently trying to print the string "I was born in Bolton" but it isn't working...
-// what's the error ?
+const cardNumber = 4533787178994213;
+const last4Digits = cardNumber.slice(-4);
 
-const cityOfBirth = "Bolton";
-console.log(`I was born in ${cityOfBirth}`);
+// The last4Digits variable should store the last 4 digits of cardNumber
+// However, the code isn't working
+// Before running the code, make and explain a prediction about why the code won't work
+// Then run the code and see what error it gives.
+// Consider: Why does it give this error? Is this what I predicted? If not, what's different?
+// Then try updating the expression last4Digits is assigned to, in order to get the correct value
 
-// The error happens because cityOfBirth is used before it is defined. JavaScript 
-// runs code from top to bottom, so the variable must be declared first.
+// Prediction: The code will not work because slice() is being used on cardNumber, which is a number.
+//The slice() method only works on strings or arrays, not numbers.
+// So I predict JavaScript will throw an error saying that slice is not a function for a number.
+
