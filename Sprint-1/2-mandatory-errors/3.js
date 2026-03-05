8578bb7aad7c1a8a760ed565dac4a39872b9d5cf
Shuheda <shuheda.hatun@gmail.com>
2026-03-05 11:53:14 +0000

    Update 3.js
    I updated the code and ran it to test. The code worked and logged the last for digits of the card, 4213.

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,5 @@
-const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+// const cardNumber = 4533787178994213;
+// const last4Digits = cardNumber.slice(-4);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -9,6 +9,12 @@ const last4Digits = cardNumber.slice(-4);
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
 
 // Prediction: The code will not work because slice() is being used on cardNumber, which is a number.
-//The slice() method only works on strings or arrays, not numbers.
+// The slice() method only works on strings or arrays, not numbers.
 // So I predict JavaScript will throw an error saying that slice is not a function for a number.
+
+const cardNumber = 4533787178994213;
+const last4Digits = cardNumber.toString().slice(-4);
+console.log(last4Digits);
+
+// After running the code, I got the output "4213", which is the last 4 digits of the card number.
 
